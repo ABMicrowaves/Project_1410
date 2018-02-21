53b75c2a67bec189b26ccdc348ef79aa815130e4
DESKTOP-0RF760O\User <roee.zinoue@comm-it.com>
2018-02-21 12:04:29 +0200

    no message

--- BA1410TX/BA1410TX1.X/nbproject/Makefile-default.mk
+++ BA1410TX/BA1410TX1.X/nbproject/Makefile-default.mk
@@ -57,17 +57,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=../../Project_1410/BA1410TX/BA1410TX.c ../AD5312.c ../AD9746.c ../ADF4350.c ../BA1410TX_intr.c ../BA1410TX_serial.c ../loader.c
+SOURCEFILES_QUOTED_IF_SPACED=../BA1410TX.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/_ext/1092510196/BA1410TX.o ${OBJECTDIR}/_ext/1472/AD5312.o ${OBJECTDIR}/_ext/1472/AD9746.o ${OBJECTDIR}/_ext/1472/ADF4350.o ${OBJECTDIR}/_ext/1472/BA1410TX_intr.o ${OBJECTDIR}/_ext/1472/BA1410TX_serial.o ${OBJECTDIR}/_ext/1472/loader.o
-POSSIBLE_DEPFILES=${OBJECTDIR}/_ext/1092510196/BA1410TX.o.d ${OBJECTDIR}/_ext/1472/AD5312.o.d ${OBJECTDIR}/_ext/1472/AD9746.o.d ${OBJECTDIR}/_ext/1472/ADF4350.o.d ${OBJECTDIR}/_ext/1472/BA1410TX_intr.o.d ${OBJECTDIR}/_ext/1472/BA1410TX_serial.o.d ${OBJECTDIR}/_ext/1472/loader.o.d
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/_ext/1472/BA1410TX.o
+POSSIBLE_DEPFILES=${OBJECTDIR}/_ext/1472/BA1410TX.o.d
 
 # Object Files
-OBJECTFILES=${OBJECTDIR}/_ext/1092510196/BA1410TX.o ${OBJECTDIR}/_ext/1472/AD5312.o ${OBJECTDIR}/_ext/1472/AD9746.o ${OBJECTDIR}/_ext/1472/ADF4350.o ${OBJECTDIR}/_ext/1472/BA1410TX_intr.o ${OBJECTDIR}/_ext/1472/BA1410TX_serial.o ${OBJECTDIR}/_ext/1472/loader.o
+OBJECTFILES=${OBJECTDIR}/_ext/1472/BA1410TX.o
 
 # Source Files
-SOURCEFILES=../../Project_1410/BA1410TX/BA1410TX.c ../AD5312.c ../AD9746.c ../ADF4350.c ../BA1410TX_intr.c ../BA1410TX_serial.c ../loader.c
+SOURCEFILES=../BA1410TX.c
 
 
 CFLAGS=
@@ -94,172 +94,28 @@ MP_LD="C:\PROGRA~2\PICC\CCSCON.exe"
 # ------------------------------------------------------------------------------------
 # Rules for buildStep: compile
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
-${OBJECTDIR}/_ext/1092510196/BA1410TX.o: ../../Project_1410/BA1410TX/BA1410TX.c  nbproject/Makefile-${CND_CONF}.mk
-	${MKDIR} ${OBJECTDIR}/_ext/1092510196 
-ifeq (7,1) 
-	${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
-	${MP_CC}  out="${OBJECTDIR}\_ext\1092510196"  ../../Project_1410/BA1410TX/BA1410TX.c +FH +DF +CC  #__DEBUG=1 +ICD +Y=9 +EA +DF +LN +T +A +M +J +EA +Z -P #__18F45K22=1 
-	@mv ${OBJECTDIR}/_ext/1092510196/BA1410TX.cof "dist/${CND_CONF}/${IMAGE_TYPE}/BA1410TX1.X.${IMAGE_TYPE}.cof" 
-	@mv ${OBJECTDIR}/_ext/1092510196/BA1410TX.hex "dist/${CND_CONF}/${IMAGE_TYPE}/BA1410TX1.X.${IMAGE_TYPE}.hex"
-else 
-	${MP_CC}  out=""${OBJECTDIR}\_ext\1092510196"" ../../Project_1410/BA1410TX/BA1410TX.c +EXPORT +FH +DF +CC  #__DEBUG=1 +ICD +Y=9 +EA +DF +LN +T +A +M +J +EA +Z -P #__18F45K22=1 +EXPORTD="${OBJECTDIR}/_ext/1092510196"  
-	
-endif 
-	
-${OBJECTDIR}/_ext/1472/AD5312.o: ../AD5312.c  nbproject/Makefile-${CND_CONF}.mk
+${OBJECTDIR}/_ext/1472/BA1410TX.o: ../BA1410TX.c  nbproject/Makefile-${CND_CONF}.mk
 	${MKDIR} ${OBJECTDIR}/_ext/1472 
-ifeq (7,1) 
+ifeq (1,1) 
 	${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
-	${MP_CC}  out="${OBJECTDIR}\_ext\1472"  ../AD5312.c +FH +DF +CC  #__DEBUG=1 +ICD +Y=9 +EA +DF +LN +T +A +M +J +EA +Z -P #__18F45K22=1 
-	@mv ${OBJECTDIR}/_ext/1472/AD5312.cof "dist/${CND_CONF}/${IMAGE_TYPE}/BA1410TX1.X.${IMAGE_TYPE}.cof" 
-	@mv ${OBJECTDIR}/_ext/1472/AD5312.hex "dist/${CND_CONF}/${IMAGE_TYPE}/BA1410TX1.X.${IMAGE_TYPE}.hex"
+	${MP_CC}  out="${OBJECTDIR}\_ext\1472"  ../BA1410TX.c +FH +DF +CC  #__DEBUG=1 +ICD +Y=9 +EA +DF +LN +T +A +M +J +EA +Z -P #__18F45K22=1 
+	@mv ${OBJECTDIR}/_ext/1472/BA1410TX.cof "dist/${CND_CONF}/${IMAGE_TYPE}/BA1410TX1.X.${IMAGE_TYPE}.cof" 
+	@mv ${OBJECTDIR}/_ext/1472/BA1410TX.hex "dist/${CND_CONF}/${IMAGE_TYPE}/BA1410TX1.X.${IMAGE_TYPE}.hex"
 else 
-	${MP_CC}  out=""${OBJECTDIR}\_ext\1472"" ../AD5312.c +EXPORT +FH +DF +CC  #__DEBUG=1 +ICD +Y=9 +EA +DF +LN +T +A +M +J +EA +Z -P #__18F45K22=1 +EXPORTD="${OBJECTDIR}/_ext/1472"  
-	
-endif 
-	
-${OBJECTDIR}/_ext/1472/AD9746.o: ../AD9746.c  nbproject/Makefile-${CND_CONF}.mk
-	${MKDIR} ${OBJECTDIR}/_ext/1472 
-ifeq (7,1) 
-	${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
-	${MP_CC}  out="${OBJECTDIR}\_ext\1472"  ../AD9746.c +FH +DF +CC  #__DEBUG=1 +ICD +Y=9 +EA +DF +LN +T +A +M +J +EA +Z -P #__18F45K22=1 
-	@mv ${OBJECTDIR}/_ext/1472/AD9746.cof "dist/${CND_CONF}/${IMAGE_TYPE}/BA1410TX1.X.${IMAGE_TYPE}.cof" 
-	@mv ${OBJECTDIR}/_ext/1472/AD9746.hex "dist/${CND_CONF}/${IMAGE_TYPE}/BA1410TX1.X.${IMAGE_TYPE}.hex"
-else 
-	${MP_CC}  out=""${OBJECTDIR}\_ext\1472"" ../AD9746.c +EXPORT +FH +DF +CC  #__DEBUG=1 +ICD +Y=9 +EA +DF +LN +T +A +M +J +EA +Z -P #__18F45K22=1 +EXPORTD="${OBJECTDIR}/_ext/1472"  
-	
-endif 
-	
-${OBJECTDIR}/_ext/1472/ADF4350.o: ../ADF4350.c  nbproject/Makefile-${CND_CONF}.mk
-	${MKDIR} ${OBJECTDIR}/_ext/1472 
-ifeq (7,1) 
-	${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
-	${MP_CC}  out="${OBJECTDIR}\_ext\1472"  ../ADF4350.c +FH +DF +CC  #__DEBUG=1 +ICD +Y=9 +EA +DF +LN +T +A +M +J +EA +Z -P #__18F45K22=1 
-	@mv ${OBJECTDIR}/_ext/1472/ADF4350.cof "dist/${CND_CONF}/${IMAGE_TYPE}/BA1410TX1.X.${IMAGE_TYPE}.cof" 
-	@mv ${OBJECTDIR}/_ext/1472/ADF4350.hex "dist/${CND_CONF}/${IMAGE_TYPE}/BA1410TX1.X.${IMAGE_TYPE}.hex"
-else 
-	${MP_CC}  out=""${OBJECTDIR}\_ext\1472"" ../ADF4350.c +EXPORT +FH +DF +CC  #__DEBUG=1 +ICD +Y=9 +EA +DF +LN +T +A +M +J +EA +Z -P #__18F45K22=1 +EXPORTD="${OBJECTDIR}/_ext/1472"  
-	
-endif 
-	
-${OBJECTDIR}/_ext/1472/BA1410TX_intr.o: ../BA1410TX_intr.c  nbproject/Makefile-${CND_CONF}.mk
-	${MKDIR} ${OBJECTDIR}/_ext/1472 
-ifeq (7,1) 
-	${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
-	${MP_CC}  out="${OBJECTDIR}\_ext\1472"  ../BA1410TX_intr.c +FH +DF +CC  #__DEBUG=1 +ICD +Y=9 +EA +DF +LN +T +A +M +J +EA +Z -P #__18F45K22=1 
-	@mv ${OBJECTDIR}/_ext/1472/BA1410TX_intr.cof "dist/${CND_CONF}/${IMAGE_TYPE}/BA1410TX1.X.${IMAGE_TYPE}.cof" 
-	@mv ${OBJECTDIR}/_ext/1472/BA1410TX_intr.hex "dist/${CND_CONF}/${IMAGE_TYPE}/BA1410TX1.X.${IMAGE_TYPE}.hex"
-else 
-	${MP_CC}  out=""${OBJECTDIR}\_ext\1472"" ../BA1410TX_intr.c +EXPORT +FH +DF +CC  #__DEBUG=1 +ICD +Y=9 +EA +DF +LN +T +A +M +J +EA +Z -P #__18F45K22=1 +EXPORTD="${OBJECTDIR}/_ext/1472"  
-	
-endif 
-	
-${OBJECTDIR}/_ext/1472/BA1410TX_serial.o: ../BA1410TX_serial.c  nbproject/Makefile-${CND_CONF}.mk
-	${MKDIR} ${OBJECTDIR}/_ext/1472 
-ifeq (7,1) 
-	${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
-	${MP_CC}  out="${OBJECTDIR}\_ext\1472"  ../BA1410TX_serial.c +FH +DF +CC  #__DEBUG=1 +ICD +Y=9 +EA +DF +LN +T +A +M +J +EA +Z -P #__18F45K22=1 
-	@mv ${OBJECTDIR}/_ext/1472/BA1410TX_serial.cof "dist/${CND_CONF}/${IMAGE_TYPE}/BA1410TX1.X.${IMAGE_TYPE}.cof" 
-	@mv ${OBJECTDIR}/_ext/1472/BA1410TX_serial.hex "dist/${CND_CONF}/${IMAGE_TYPE}/BA1410TX1.X.${IMAGE_TYPE}.hex"
-else 
-	${MP_CC}  out=""${OBJECTDIR}\_ext\1472"" ../BA1410TX_serial.c +EXPORT +FH +DF +CC  #__DEBUG=1 +ICD +Y=9 +EA +DF +LN +T +A +M +J +EA +Z -P #__18F45K22=1 +EXPORTD="${OBJECTDIR}/_ext/1472"  
-	
-endif 
-	
-${OBJECTDIR}/_ext/1472/loader.o: ../loader.c  nbproject/Makefile-${CND_CONF}.mk
-	${MKDIR} ${OBJECTDIR}/_ext/1472 
-ifeq (7,1) 
-	${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
-	${MP_CC}  out="${OBJECTDIR}\_ext\1472"  ../loader.c +FH +DF +CC  #__DEBUG=1 +ICD +Y=9 +EA +DF +LN +T +A +M +J +EA +Z -P #__18F45K22=1 
-	@mv ${OBJECTDIR}/_ext/1472/loader.cof "dist/${CND_CONF}/${IMAGE_TYPE}/BA1410TX1.X.${IMAGE_TYPE}.cof" 
-	@mv ${OBJECTDIR}/_ext/1472/loader.hex "dist/${CND_CONF}/${IMAGE_TYPE}/BA1410TX1.X.${IMAGE_TYPE}.hex"
-else 
-	${MP_CC}  out=""${OBJECTDIR}\_ext\1472"" ../loader.c +EXPORT +FH +DF +CC  #__DEBUG=1 +ICD +Y=9 +EA +DF +LN +T +A +M +J +EA +Z -P #__18F45K22=1 +EXPORTD="${OBJECTDIR}/_ext/1472"  
+	${MP_CC}  out=""${OBJECTDIR}\_ext\1472"" ../BA1410TX.c +EXPORT +FH +DF +CC  #__DEBUG=1 +ICD +Y=9 +EA +DF +LN +T +A +M +J +EA +Z -P #__18F45K22=1 +EXPORTD="${OBJECTDIR}/_ext/1472"  
 	
 endif 
 	
 else
-${OBJECTDIR}/_ext/1092510196/BA1410TX.o: ../../Project_1410/BA1410TX/BA1410TX.c  nbproject/Makefile-${CND_CONF}.mk
-	${MKDIR} ${OBJECTDIR}/_ext/1092510196 
-ifeq (7,1) 
-	${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
-	${MP_CC}  out="${OBJECTDIR}\_ext\1092510196"  ../../Project_1410/BA1410TX/BA1410TX.c +FH +DF +CC +Y=9 +EA +DF +LN +T +A +M +J +EA +Z -P #__18F45K22=1 
-	@mv ${OBJECTDIR}/_ext/1092510196/BA1410TX.cof "dist/${CND_CONF}/${IMAGE_TYPE}/BA1410TX1.X.${IMAGE_TYPE}.cof" 
-	@mv ${OBJECTDIR}/_ext/1092510196/BA1410TX.hex "dist/${CND_CONF}/${IMAGE_TYPE}/BA1410TX1.X.${IMAGE_TYPE}.hex"
-else 
-	${MP_CC}  out=""${OBJECTDIR}\_ext\1092510196"" ../../Project_1410/BA1410TX/BA1410TX.c +EXPORT +FH +DF +CC +Y=9 +EA +DF +LN +T +A +M +J +EA +Z -P #__18F45K22=1 +EXPORTD="${OBJECTDIR}/_ext/1092510196"  
-	
-endif 
-	
-${OBJECTDIR}/_ext/1472/AD5312.o: ../AD5312.c  nbproject/Makefile-${CND_CONF}.mk
+${OBJECTDIR}/_ext/1472/BA1410TX.o: ../BA1410TX.c  nbproject/Makefile-${CND_CONF}.mk
 	${MKDIR} ${OBJECTDIR}/_ext/1472 
-ifeq (7,1) 
+ifeq (1,1) 
 	${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
-	${MP_CC}  out="${OBJECTDIR}\_ext\1472"  ../AD5312.c +FH +DF +CC +Y=9 +EA +DF +LN +T +A +M +J +EA +Z -P #__18F45K22=1 
-	@mv ${OBJECTDIR}/_ext/1472/AD5312.cof "dist/${CND_CONF}/${IMAGE_TYPE}/BA1410TX1.X.${IMAGE_TYPE}.cof" 
-	@mv ${OBJECTDIR}/_ext/1472/AD5312.hex "dist/${CND_CONF}/${IMAGE_TYPE}/BA1410TX1.X.${IMAGE_TYPE}.hex"
+	${MP_CC}  out="${OBJECTDIR}\_ext\1472"  ../BA1410TX.c +FH +DF +CC +Y=9 +EA +DF +LN +T +A +M +J +EA +Z -P #__18F45K22=1 
+	@mv ${OBJECTDIR}/_ext/1472/BA1410TX.cof "dist/${CND_CONF}/${IMAGE_TYPE}/BA1410TX1.X.${IMAGE_TYPE}.cof" 
+	@mv ${OBJECTDIR}/_ext/1472/BA1410TX.hex "dist/${CND_CONF}/${IMAGE_TYPE}/BA1410TX1.X.${IMAGE_TYPE}.hex"
 else 
-	${MP_CC}  out=""${OBJECTDIR}\_ext\1472"" ../AD5312.c +EXPORT +FH +DF +CC +Y=9 +EA +DF +LN +T +A +M +J +EA +Z -P #__18F45K22=1 +EXPORTD="${OBJECTDIR}/_ext/1472"  
-	
-endif 
-	
-${OBJECTDIR}/_ext/1472/AD9746.o: ../AD9746.c  nbproject/Makefile-${CND_CONF}.mk
-	${MKDIR} ${OBJECTDIR}/_ext/1472 
-ifeq (7,1) 
-	${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
-	${MP_CC}  out="${OBJECTDIR}\_ext\1472"  ../AD9746.c +FH +DF +CC +Y=9 +EA +DF +LN +T +A +M +J +EA +Z -P #__18F45K22=1 
-	@mv ${OBJECTDIR}/_ext/1472/AD9746.cof "dist/${CND_CONF}/${IMAGE_TYPE}/BA1410TX1.X.${IMAGE_TYPE}.cof" 
-	@mv ${OBJECTDIR}/_ext/1472/AD9746.hex "dist/${CND_CONF}/${IMAGE_TYPE}/BA1410TX1.X.${IMAGE_TYPE}.hex"
-else 
-	${MP_CC}  out=""${OBJECTDIR}\_ext\1472"" ../AD9746.c +EXPORT +FH +DF +CC +Y=9 +EA +DF +LN +T +A +M +J +EA +Z -P #__18F45K22=1 +EXPORTD="${OBJECTDIR}/_ext/1472"  
-	
-endif 
-	
-${OBJECTDIR}/_ext/1472/ADF4350.o: ../ADF4350.c  nbproject/Makefile-${CND_CONF}.mk
-	${MKDIR} ${OBJECTDIR}/_ext/1472 
-ifeq (7,1) 
-	${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
-	${MP_CC}  out="${OBJECTDIR}\_ext\1472"  ../ADF4350.c +FH +DF +CC +Y=9 +EA +DF +LN +T +A +M +J +EA +Z -P #__18F45K22=1 
-	@mv ${OBJECTDIR}/_ext/1472/ADF4350.cof "dist/${CND_CONF}/${IMAGE_TYPE}/BA1410TX1.X.${IMAGE_TYPE}.cof" 
-	@mv ${OBJECTDIR}/_ext/1472/ADF4350.hex "dist/${CND_CONF}/${IMAGE_TYPE}/BA1410TX1.X.${IMAGE_TYPE}.hex"
-else 
-	${MP_CC}  out=""${OBJECTDIR}\_ext\1472"" ../ADF4350.c +EXPORT +FH +DF +CC +Y=9 +EA +DF +LN +T +A +M +J +EA +Z -P #__18F45K22=1 +EXPORTD="${OBJECTDIR}/_ext/1472"  
-	
-endif 
-	
-${OBJECTDIR}/_ext/1472/BA1410TX_intr.o: ../BA1410TX_intr.c  nbproject/Makefile-${CND_CONF}.mk
-	${MKDIR} ${OBJECTDIR}/_ext/1472 
-ifeq (7,1) 
-	${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
-	${MP_CC}  out="${OBJECTDIR}\_ext\1472"  ../BA1410TX_intr.c +FH +DF +CC +Y=9 +EA +DF +LN +T +A +M +J +EA +Z -P #__18F45K22=1 
-	@mv ${OBJECTDIR}/_ext/1472/BA1410TX_intr.cof "dist/${CND_CONF}/${IMAGE_TYPE}/BA1410TX1.X.${IMAGE_TYPE}.cof" 
-	@mv ${OBJECTDIR}/_ext/1472/BA1410TX_intr.hex "dist/${CND_CONF}/${IMAGE_TYPE}/BA1410TX1.X.${IMAGE_TYPE}.hex"
-else 
-	${MP_CC}  out=""${OBJECTDIR}\_ext\1472"" ../BA1410TX_intr.c +EXPORT +FH +DF +CC +Y=9 +EA +DF +LN +T +A +M +J +EA +Z -P #__18F45K22=1 +EXPORTD="${OBJECTDIR}/_ext/1472"  
-	
-endif 
-	
-${OBJECTDIR}/_ext/1472/BA1410TX_serial.o: ../BA1410TX_serial.c  nbproject/Makefile-${CND_CONF}.mk
-	${MKDIR} ${OBJECTDIR}/_ext/1472 
-ifeq (7,1) 
-	${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
-	${MP_CC}  out="${OBJECTDIR}\_ext\1472"  ../BA1410TX_serial.c +FH +DF +CC +Y=9 +EA +DF +LN +T +A +M +J +EA +Z -P #__18F45K22=1 
-	@mv ${OBJECTDIR}/_ext/1472/BA1410TX_serial.cof "dist/${CND_CONF}/${IMAGE_TYPE}/BA1410TX1.X.${IMAGE_TYPE}.cof" 
-	@mv ${OBJECTDIR}/_ext/1472/BA1410TX_serial.hex "dist/${CND_CONF}/${IMAGE_TYPE}/BA1410TX1.X.${IMAGE_TYPE}.hex"
-else 
-	${MP_CC}  out=""${OBJECTDIR}\_ext\1472"" ../BA1410TX_serial.c +EXPORT +FH +DF +CC +Y=9 +EA +DF +LN +T +A +M +J +EA +Z -P #__18F45K22=1 +EXPORTD="${OBJECTDIR}/_ext/1472"  
-	
-endif 
-	
-${OBJECTDIR}/_ext/1472/loader.o: ../loader.c  nbproject/Makefile-${CND_CONF}.mk
-	${MKDIR} ${OBJECTDIR}/_ext/1472 
-ifeq (7,1) 
-	${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
-	${MP_CC}  out="${OBJECTDIR}\_ext\1472"  ../loader.c +FH +DF +CC +Y=9 +EA +DF +LN +T +A +M +J +EA +Z -P #__18F45K22=1 
-	@mv ${OBJECTDIR}/_ext/1472/loader.cof "dist/${CND_CONF}/${IMAGE_TYPE}/BA1410TX1.X.${IMAGE_TYPE}.cof" 
-	@mv ${OBJECTDIR}/_ext/1472/loader.hex "dist/${CND_CONF}/${IMAGE_TYPE}/BA1410TX1.X.${IMAGE_TYPE}.hex"
-else 
-	${MP_CC}  out=""${OBJECTDIR}\_ext\1472"" ../loader.c +EXPORT +FH +DF +CC +Y=9 +EA +DF +LN +T +A +M +J +EA +Z -P #__18F45K22=1 +EXPORTD="${OBJECTDIR}/_ext/1472"  
+	${MP_CC}  out=""${OBJECTDIR}\_ext\1472"" ../BA1410TX.c +EXPORT +FH +DF +CC +Y=9 +EA +DF +LN +T +A +M +J +EA +Z -P #__18F45K22=1 +EXPORTD="${OBJECTDIR}/_ext/1472"  
 	
 endif 
 	
@@ -270,20 +126,20 @@ endif
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
 dist/${CND_CONF}/${IMAGE_TYPE}/BA1410TX1.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk    
 	${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
-ifeq (7,1) 
+ifeq (1,1) 
 	
 else 
-	${MP_LD}   out="dist/${CND_CONF}/${IMAGE_TYPE}"  +FH +DF +CC LINK=BA1410TX1.X.${IMAGE_TYPE}.hex=${OBJECTDIR}/_ext/1092510196/BA1410TX.o,${OBJECTDIR}/_ext/1472/AD5312.o,${OBJECTDIR}/_ext/1472/AD9746.o,${OBJECTDIR}/_ext/1472/ADF4350.o,${OBJECTDIR}/_ext/1472/BA1410TX_intr.o,${OBJECTDIR}/_ext/1472/BA1410TX_serial.o,${OBJECTDIR}/_ext/1472/loader.o +Y=9 +EA +DF +LN +T +A +M +J +EA +Z -P
+	${MP_LD}   out="dist/${CND_CONF}/${IMAGE_TYPE}"  +FH +DF +CC LINK=BA1410TX1.X.${IMAGE_TYPE}.hex=${OBJECTDIR}/_ext/1472/BA1410TX.o +Y=9 +EA +DF +LN +T +A +M +J +EA +Z -P
 	
 endif 
 	
 else
 dist/${CND_CONF}/${IMAGE_TYPE}/BA1410TX1.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk   
 	${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
-ifeq (7,1) 
+ifeq (1,1) 
 	
 else 
-	${MP_LD}   out="dist/${CND_CONF}/${IMAGE_TYPE}"  +FH +DF +CC LINK=BA1410TX1.X.${IMAGE_TYPE}.hex=${OBJECTDIR}/_ext/1092510196/BA1410TX.o,${OBJECTDIR}/_ext/1472/AD5312.o,${OBJECTDIR}/_ext/1472/AD9746.o,${OBJECTDIR}/_ext/1472/ADF4350.o,${OBJECTDIR}/_ext/1472/BA1410TX_intr.o,${OBJECTDIR}/_ext/1472/BA1410TX_serial.o,${OBJECTDIR}/_ext/1472/loader.o +Y=9 +EA +DF +LN +T +A +M +J +EA +Z -P
+	${MP_LD}   out="dist/${CND_CONF}/${IMAGE_TYPE}"  +FH +DF +CC LINK=BA1410TX1.X.${IMAGE_TYPE}.hex=${OBJECTDIR}/_ext/1472/BA1410TX.o +Y=9 +EA +DF +LN +T +A +M +J +EA +Z -P
 	
 endif 
 	
